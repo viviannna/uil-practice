8a6aeafed9fcf73a3d578e4b285617bdeddef93e
vivianna <60679089+viviannna@users.noreply.github.com>
2021-11-08 22:40:31 -0600

    Update Adela.java

--- 02-2021-regionals/q1-adela/Adela.java
+++ 02-2021-regionals/q1-adela/Adela.java
@@ -1,3 +1,5 @@
+// CONCLUSION: You COULD do it with loops, but just don't. It's much faster to just manually print it out.
+
 public class Adela {
     public static void main(String[] args) {
         
@@ -12,6 +14,7 @@ public class Adela {
         }
 
         // bottom half (10-17)
+        // this needs to be fixed, not working
         // for (int rowNumber = 10; rowNumber <=17; rowNumber ++) {
         //     for (int astInRow = 8; astInRow >= 1; astInRow-=1) {
         //         System.out.print("*");
