3ecd5465a8725a8ca7fb1500ad4779b420f38468
Vivianna Lieu <lieu.vivianna@gmail.com>
2022-01-13 11:26:06 -0600

    q7 in progress

---
+++ general-techniques/string-to-list/Convert.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+import java.util.List;
+import java.util.Arrays;
+
+
+public class Convert {
+
+    public static void main(String[] args ) {
+
+        // testing turning a string into an array 
+        String stringOfNums = "2,3,11,12";
+        String str[] = stringOfNums.split(",");
+        List<String> al = new ArrayList<String>();
+        al = Arrays.asList(str);
+
+        System.out.println(al);
+
+
+
+
+    }
+    
+}
